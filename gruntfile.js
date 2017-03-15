5508422e7b4fc79d6a97a43012998d531965e9cb
mrijalul <muhammadrijalulmutaqin@gmail.com>
2017-03-15 11:55:41 +0700

    first commit

---
+++ gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt)
+{
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		image: {
+			dynamic: {
+				options: {
+					pngquant: true,
+					optipng: false,
+					zopflipng: true,
+					jpegRecompress: false,
+					jpegoptim: true,
+					mozjpeg: true,
+					gifsicle: true,
+					svgo: true
+				},
+				files: [{
+					expand: true,
+					cwd: 'images/',
+					src: ['**/*.{png,jpg,jpeg,gif}'],
+					dest: 'img-compress/'
+				}]
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.loadNpmTasks('grunt-image');
+};
